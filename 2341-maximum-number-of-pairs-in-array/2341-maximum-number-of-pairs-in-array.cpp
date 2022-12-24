54ea8e4e5d88eda74a22c3d41cc75efc5d172d79
Aniket Sinha <sinhaaniket192@gmail.com>
2022-12-24 21:43:35 +0530

    Time: 4 ms (66.94%), Space: 9.3 MB (53.79%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
@@ -2,31 +2,12 @@ class Solution {
 public:
     vector<int> numberOfPairs(vector<int>& nums) {
         
-        int n = nums.size();
-        int count=0;
-        int left=0;
+        int n = nums.size(),count=0,left=0;
         
-        // vector<int> hash(n+1);
         map<int,int> mp;
         
-        vector<int> ans;
-        
-        for(int i=0;i<n;i++)
-            mp[nums[i]]++;
-        
-        
-        
-//         for(int j=0;j<hash.size();j++)
-//             {
-//                 if(hash[j]%2==0) count=count+hash[j]/2;
-//                 else 
-//                 {
-//                     left=left+hash[j]%2;
-//                     count=count+hash[j]/2;
-//                 }
-//             }
-        
-        
+        for(int i=0;i<n;i++) mp[nums[i]]++;       
+
         for(auto it : mp)
             {
                 if(it.second%2==0) count=count+it.second/2;
@@ -35,16 +16,7 @@ public:
                     left=left+it.second%2;
                     count=count+it.second/2;
                 }
-            }
-        
-            
-        ans.push_back(count);ans.push_back(left);
-        
-        
-        // 1 - 2
-        // 3 - 2
-        // 2 - 3
-            
-            return ans;     
+            }  
+        return {count,left};     
     }
 };
