aac405988d8f29c6a8dbcbf0c00cb42f05a1e50e
Aniket Sinha <sinhaaniket192@gmail.com>
2022-12-28 03:22:00 +0530

    Time: 6 ms (98.49%), Space: 10.5 MB (94.08%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+        int m = nums.size();
+        int n = nums[0].size();
+        if (m * n != r * c) {
+            return nums;
+        }
+        vector<vector<int>> res(r, vector<int>(c));
+        int row = 0;
+        int col = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[row][col] = nums[i][j];
+                col++;
+                if (col == c) {
+                    row++;
+                    col = 0;
+                }
+            }
+        }
+        return res;
+    }
+};
