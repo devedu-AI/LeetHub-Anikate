4a3cfcde60798a3b29d273247d5989912f4717bb
Aniket Sinha <sinhaaniket192@gmail.com>
2023-01-01 13:55:48 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (82.12%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
-    bool wordPattern(string pattern, string s) {
+    bool wordPattern(string pattern, string s) 
+    {     
+        int i = 0, n = pattern.size();
         unordered_map<char, int> p2i;
         unordered_map<string, int> w2i;
-        
-        istringstream in(s); string word;
-        int i = 0, n = pattern.size();
 
+        // Breaking input into word
+        // using string stream
         
-        for(word; in>>word; i++){
+        istringstream in(s); // Used for breaking words
+        
+        string word; // To store individual words
+
+        for(word; in>>word; i++)
+        {
             if(i==n || p2i[pattern[i]] != w2i[word]) return false; //If it reaches end before all the words in string 's' are traversed || if values of keys : pattern[i] & word don't match return false
             
             p2i[pattern[i]] = w2i[word] = i+1; //Otherwise map to both to a value i+1
