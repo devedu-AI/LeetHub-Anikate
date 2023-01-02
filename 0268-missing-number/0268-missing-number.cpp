1bb76cbe3477a5c08e3afb31c961622567f37d88
Aniket Sinha <sinhaaniket192@gmail.com>
2023-01-03 02:43:09 +0530

    Time: 30 ms (61.59%), Space: 17.9 MB (67.07%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-    
-        int i;
-        
-        for(i=0;i<=nums.size();i++)
-        {
-            if (find(nums.begin(), nums.end(), i) != nums.end() ) // item present 
-                   continue;
-            
-            else
-            {
-                return i; break; // else if item not present return
-            }
-        }
-        
-        return i;
+        return nums.size()*(nums.size()+1)/2 - accumulate(nums.begin(),nums.end(),0);
     }
 };
