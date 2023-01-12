af2ec3a46c7922370e737454b884ce2059888036
Aniket Sinha <sinhaaniket192@gmail.com>
2023-01-13 01:10:33 +0530

    Added solution - LeetHub

---
+++ Introduction to Linked List - GFG/introduction-to-linked-list.cpp
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+  
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+  
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/*class Node {
+public:
+    int data;
+    Node* next;
+  
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+  
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* constructLL(vector<int>& arr) 
+    {
+        int n= arr.size() ;
+        Node* head = new Node(arr[0]) ; //Make the first element as the head of the list
+        Node* last_node = head;         //Make var to store the last node
+        
+        for(int i=1; i<n && last_node!=NULL;i++)
+        {   //traverse from second to last element
+            Node* new_node = new Node(arr[i]) ;     // Make new node and store the data of array
+            last_node->next = new_node ;    // link the last node to new node
+            last_node = new_node;   // The new node becomes last node.
+        }
+        
+        return head ;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        Node* ans = obj.constructLL(arr);
+        while (ans) {
+            cout << ans->data << " ";
+            ans = ans->next;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
